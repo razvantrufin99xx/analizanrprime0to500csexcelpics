171885778ff6666c4125e636ceae37c8d44dd2db
sibiu <144681144+razvantrufin99xx@users.noreply.github.com>
2024-09-26 08:03:36 +0300

    Add files via upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,101 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: razvan
+ * Date: 9/26/2024
+ * Time: 6:55 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace formuleDiverseMatematica
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Button button2;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(37, 39);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(100, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "text x";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(37, 69);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 1;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(217, 39);
+			this.textBox2.Multiline = true;
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(436, 481);
+			this.textBox2.TabIndex = 2;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(37, 158);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(100, 23);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "text range";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(733, 564);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.Name = "MainForm";
+			this.Text = "formuleDiverseMatematica";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,110 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: razvan
+ * Date: 9/26/2024
+ * Time: 6:55 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace formuleDiverseMatematica
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			
+		}
+		public class returnboolint 
+		{
+			public bool x;
+			public int i;
+			public returnboolint(bool px, int pi)
+			{
+				this.x = px;
+				this.i = pi;
+			}
+		}
+		public returnboolint isPrime(int x)
+		{
+			
+			for(int i = 2 ; i< x/2 ; i++)
+			{
+				if(x%i==0){return new returnboolint(false, i);}
+			}
+			return new returnboolint(true, x);
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			int v = 0;
+			try{
+				v = int.Parse(this.textBox1.Text);
+			}
+			catch{};
+			
+			textBox2.Text ="";
+			returnboolint r = isPrime(v);
+			
+			bool b = r.x;
+			int i = r.i;
+			
+			if(r.x==true)
+			{
+				textBox2.Text += v.ToString() + " \r\t" + " is prime " + i.ToString() ;
+					
+			}
+			else
+			{
+				textBox2.Text += v.ToString() + " \r\t" + " is not prime " + i.ToString() ;
+					
+			}
+		}
+		
+		public void testknr()
+		{
+			for(int v = 0 ; v < 500; v++){
+			
+			
+			returnboolint r = isPrime(v);
+			
+			bool b = r.x;
+			int i = r.i;
+			
+			if(r.x==true)
+			{
+				textBox2.Text += v.ToString() + "\r\t" + " is prime " + i.ToString() ;
+					
+			}
+			else
+			{
+				textBox2.Text += v.ToString() + " \r\t" + " is not prime " + i.ToString() ;
+					
+			}
+			textBox2.Refresh();
+			textBox2.Text += "\r\n";
+			}
+		}
+		void Button2Click(object sender, EventArgs e)
+		{
+			testknr();
+		}
+	}
+}
